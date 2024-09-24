a52cd456b581663d05497e7250567886c84e034d
geekact <geekacting@gmail.com>
2024-09-25 00:44:34 +0800

    refactor(node): i18nProvider移动到I18n静态属性

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -3,28 +3,28 @@ import { AsyncLocalStorage } from 'node:async_hooks';
 
 const storage = new AsyncLocalStorage<string>();
 
-/**
- * 语言上下文，执行的任何翻译都会使用当前提供的语言。建议使用在中间件
- * ```typescript
- * function myMiddleware(ctx, next) {
- *   const language = 'zh-CN';
- *   return i18nProvider(language, next);
- * }
- * ```
- */
-export const i18nProvider = <T>(language: string, callback: () => Promise<T>) => {
-  return new Promise<T>((resolve, reject) => {
-    storage.run(language, () => {
-      callback().then(resolve).catch(reject);
-    });
-  });
-};
-
 export class I18n<
   Locales extends Record<string, object>,
   Languages extends string & keyof Locales,
   DefaultLanguage extends Languages,
 > extends CoreI18n<Locales, Languages, DefaultLanguage> {
+  /**
+   * 语言上下文，执行的任何翻译都会使用当前提供的语言。建议使用在中间件
+   * ```typescript
+   * function myMiddleware(ctx, next) {
+   *   const language = 'zh-CN';
+   *   return I18n.provider(language, next);
+   * }
+   * ```
+   */
+  static provider<T>(language: string, callback: () => Promise<T>) {
+    return new Promise<T>((resolve, reject) => {
+      storage.run(language, () => {
+        callback().then(resolve).catch(reject);
+      });
+    });
+  }
+
   override get language() {
     const parentLanguage = storage.getStore() as Languages | undefined;
     return parentLanguage || this._currentLanguage;
